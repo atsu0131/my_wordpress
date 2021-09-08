63627dff745194c65917f4fe3d9d36786d902e25
atsushi.isai <atsushi_i@branch-plus.co.jp>
2021-09-08 18:56:15 +0900

    add

--- html/wp-content/themes/wpbeg/searchform.php
+++ html/wp-content/themes/wpbeg/searchform.php
@@ -1,4 +1,4 @@
-<form class="p-search-form" id="searchform" method="get" action="<?php echo esc_ur('/'); ?>">
+<form class="p-search-form" id="searchform" method="get" action="<?php echo esc_url('/'); ?>">
     <label class="screen-reader-text" for="s">検索:</label>
     <input class="p-search-form__keyword" placeholder="キーワード" name="s" id="s">
     <input class="p-search-form__submit" id="searchsubmit" type="submit" value="検索">
